e727effbe649377f1c786438769fd59f61afbe5f
Carol <carolportellazanelato@gmail.com>
2025-06-26 10:39:38 -0300

    Update Cadastro.java

--- Segunda Tentativa/AtividadeFixacao/Cadastro.java
+++ Segunda Tentativa/AtividadeFixacao/Cadastro.java
@@ -2,6 +2,8 @@ import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -9,13 +11,14 @@ import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.LookAndFeel;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.text.MaskFormatter;
 
-public class Cadastro extends JInternalFrame{
+public class Cadastro extends JInternalFrame {
     private JLabel lTil = new JLabel("Título");
     private JFormattedTextField txtTil = new JFormattedTextField();
 
@@ -23,7 +26,7 @@ public class Cadastro extends JInternalFrame{
     private JTextField txtAno = new JTextField(10);
 
     private JLabel lGen = new JLabel("Gênero");
-    String vetGen [] = {"Terror", "Ação", "Comédia", "Romance", "Suspense", "Drama"};
+    String vetGen[] = { "Terror", "Ação", "Comédia", "Romance", "Suspense", "Drama" };
     private JComboBox<String> comboGen = new JComboBox<>(vetGen);
 
     private JLabel lID = new JLabel("ID");
@@ -34,44 +37,42 @@ public class Cadastro extends JInternalFrame{
 
     private JButton bntSalvar = new JButton("Salvar");
     private JButton bntCan = new JButton("Cancelar");
+    private JButton bntEditar = new JButton("Editar");
+    private JButton bntExcluir = new JButton("Excluir");
+    private JButton bntProx = new JButton("Próximo");
+    private JButton bntAnt = new JButton("Anterior");
 
     ArrayList<Filmes> arrayFil = new ArrayList<Filmes>();
 
-    public Cadastro(){
+    int ultimo;
+    int posicao;
+
+    Persistência per = new Persistência();
+    String filename = "dados.ser";
+
+    public Cadastro() {
         super("Tela de cadastro", true, true, true, true);
         setSize(500, 300);
         setLayout(bag);
 
-        try{
+        try {
             MaskFormatter mask = new MaskFormatter("###.###-##");
             txtID = new JFormattedTextField(mask);
         }
 
-        catch(Exception e){
+        catch (Exception e) {
             System.out.println("Ocorreu o seguinte erro ao tentar adicionar a mascára " + e.getMessage());
         }
 
-        // Look and feel //
-        try{
-            for(LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()){
-                if(("GTK+").equals(info.getName())){
-                    UIManager.setLookAndFeel(info.getClassName());
-                }
-            }
-        }
-
-        catch(Exception e){
-            System.out.println("Ocorreu o seguinte erro " + e.getMessage());
-        }
-        
         // Adicionando os componentes //
         constraints.fill = GridBagConstraints.BOTH;
+        constraints.weightx = 1;
         addComponent(lTil, 0, 0, 1, 1);
         addComponent(txtTil, 1, 0, 2, 1);
 
         addComponent(lAno, 0, 1, 1, 1);
-        addComponent(txtAno, 1, 1 ,2, 1);
-        
+        addComponent(txtAno, 1, 1, 2, 1);
+
         addComponent(lID, 0, 2, 1, 1);
         addComponent(txtID, 1, 2, 1, 1);
         addComponent(comboGen, 2, 2, 1, 1);
@@ -79,28 +80,23 @@ public class Cadastro extends JInternalFrame{
         addComponent(bntSalvar, 1, 3, 1, 1);
         addComponent(bntCan, 2, 3, 1, 1);
 
+        addComponent(bntEditar, 1, 4, 1, 1);
+        addComponent(bntExcluir, 2, 4, 1, 1);
+
+        addComponent(bntProx, 1, 5, 1, 1);
+        addComponent(bntAnt, 2, 5, 1, 1);
+
+        carregarFilmes(0);
+
         setVisible(true);
 
-        // Tirando a máscara //
-        String id1 = txtID.getText().replaceAll("[. -]", "");
+        bntSalvar.addActionListener(new Eventos());
+        bntAnt.addActionListener(new Eventos());
+        bntProx.addActionListener(new Eventos());
 
-        String telefone = txtID.getText().replaceAll("[. -]","");
-
-        System.out.println("O valor do id é " + telefone);
-
-        //int id = Integer.parseInt(id1); 
-
-        //String titulo = txtTil.getText();
-        //int ano = Integer.parseInt(txtAno.getText());
-        //String genero = String.valueOf(comboGen.getSelectedItem());
-
-        // Fazendo o método salvar //
-        //Filmes filmes = new Filmes(titulo, ano, genero, id);
-
-        
     }
 
-    public void addComponent(Component c, int x, int y, int width, int height){
+    public void addComponent(Component c, int x, int y, int width, int height) {
         constraints.gridx = x;
         constraints.gridy = y;
         constraints.gridwidth = width;
@@ -109,4 +105,73 @@ public class Cadastro extends JInternalFrame{
         bag.setConstraints(c, constraints);
         add(c);
     }
+
+    private class Eventos implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            if (event.getSource() == bntSalvar) {
+                // Tirando a máscara //
+                String id1 = txtID.getText().replaceAll("[. -]", "");
+
+                int id = Integer.parseInt(id1);
+
+                String titulo = txtTil.getText();
+                int ano = Integer.parseInt(txtAno.getText());
+                String genero = String.valueOf(comboGen.getSelectedItem());
+
+                // Fazendo o método salvar //
+                Filmes filmes = new Filmes(titulo, ano, genero, id);
+
+                arrayFil.add(filmes);
+
+                per.gravarFilmes(arrayFil, filename);
+
+                ultimo = arrayFil.size() - 1;
+
+                carregarFilmes(posicao);
+            }
+
+            if(event.getSource() == bntAnt){
+                if(posicao - 1 < 0){
+                    System.out.println("Não há anteriores");
+                }
+
+                else{
+                    posicao = posicao - 1;
+                    carregarFilmes(posicao);
+                }
+            }
+
+            if(event.getSource() == bntProx){
+                if(posicao + 1 > arrayFil.size() - 1){
+                    System.out.println("Não há próximos filmes");
+                }
+
+                else{
+                    posicao = posicao + 1;
+                    carregarFilmes(posicao);
+                }
+            }
+
+        }
+
+    }
+
+    public void carregarFilmes(int posicao) {
+        if (arrayFil.isEmpty()) {
+            System.out.println("Cadastre um usuário para visualizar");
+        }
+
+        else {
+            arrayFil = per.carrgarFilmes("dados.ser");
+
+            Filmes f = arrayFil.get(posicao);
+
+            comboGen.setSelectedItem(f.getGenero());
+            txtAno.setText(String.valueOf(f.getAno()));
+            txtID.setText(String.valueOf(f.getId()));
+            txtTil.setText(String.valueOf(f.getId()));
+        }
+    }
+
 }
